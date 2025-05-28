35e7ea54f28f2570e54569eb3eac758964e99e96
mymac <mymac@mymacui-MacBookPro.local>
2025-05-28 16:32:43 +0900

    초기 커밋

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StudyTimer
+//
+//  Created by mymac on 2025/05/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
